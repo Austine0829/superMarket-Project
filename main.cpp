023b4fd27085e8caa0041a0866f77c6f4e948dec
Austine Tuazon <audreytuazon29@gmail.com>
2024-05-15 13:53:45 +0800

    done adding menu function

--- main.cpp
+++ main.cpp
@@ -4,7 +4,40 @@
 
 using namespace std;
 
+class superMarket{
+    public:
+     unique_ptr<string>unpProductName = make_unique<string>();
+     unique_ptr<int>unpProductPrice = make_unique<int>();
+     unique_ptr<int>unpProductNumber = make_unique<int>();
+
+    public:
+     void menuFunc();
+     void addProductFunc();
+};
+
 int main (){
 
+
+
     return 0;
+}
+
+void superMarket :: menuFunc(){
+
+    int choice;
+
+    cout << "Enter Choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        
+        addProductFunc();
+
+        break;
+    
+    default:
+        break;
+    }
 }
