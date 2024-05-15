9bdcf54a9c96b1f8f83ec10bed2fa80bae8f075d
Austine Tuazon <audreytuazon29@gmail.com>
2024-05-15 14:01:21 +0800

    done adding add function

--- main.cpp
+++ main.cpp
@@ -16,7 +16,9 @@ class superMarket{
 };
 
 int main (){
-
+    
+    superMarket out;
+    out.menuFunc();
 
 
     return 0;
@@ -25,7 +27,7 @@ int main (){
 void superMarket :: menuFunc(){
 
     int choice;
-
+    
     cout << "Enter Choice: ";
     cin >> choice;
 
@@ -40,4 +42,32 @@ void superMarket :: menuFunc(){
     default:
         break;
     }
+}
+
+void superMarket :: addProductFunc(){
+
+    fstream productFile;
+    
+    productFile.open("productFile.txt", ios::out | ios::app);
+
+    if (productFile.is_open())
+    {
+        cout << "Enter Product Number: ";
+        cin >> *unpProductNumber;
+
+        cout << "Enter Product Name: ";
+        cin >> *unpProductName;
+
+        cout << "Enter Product Price: ";
+        cin >> *unpProductPrice;
+
+        productFile << *unpProductNumber << " " << *unpProductName << " " << *unpProductPrice;
+    }
+
+    else
+    {
+        cout << "Error Has Occured In Add Product Function\n";
+    }
+
+    productFile.close();   
 }
